2cd4de69d297565976f44771f11233a0e3b433cc
Viet Hung <viethung01234@gmail.com>
2024-05-17 22:04:38 +0700

    [ADD]: setup toolkit, navbar, default theme

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,17 @@
-
+import { Box, Container, HStack, Image } from '@chakra-ui/react';
+import logo from '@/assets/logo.png';
 const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <Container maxW='container.xl' maxH='200px'>
+      <HStack maxH='full'>
+        <Box>
+          <Image src={logo} boxSize='180px' objectFit='cover'></Image>
+        </Box>
+        <Box>search</Box>
+        <Box>cart</Box>
+      </HStack>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
