fe2e330b19cf5ca7a970b15844459904bacf565f
Viet Hung <viethung01234@gmail.com>
2024-06-06 08:38:14 +0700

    [DELETE]: remove unused button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,13 @@
 import { useGetSellingProductQuery } from '@/apis/productApi';
-import { logout } from '@/features/Auth/authSlice';
 import GridProductList from '@/features/Product/ProductList/GridProductList';
-import { Box, Button, Container } from '@chakra-ui/react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Box, Container } from '@chakra-ui/react';
 const Home = () => {
-  const authState = useSelector(state => state.auth);
-  const dispatch = useDispatch();
   const { data = { items: [] }, isLoading } = useGetSellingProductQuery();
 
-  const handleSubmit = () => {
-    dispatch(logout());
-  };
   if (isLoading) return <p>loading..........!</p>;
 
   return (
     <Container maxW='container.xl'>
-      hello {authState?.userData?.username}
-      <Button colorScheme='red' onClick={handleSubmit}>
-        Logout
-      </Button>
       <Box>
         <GridProductList data={data} />
       </Box>
