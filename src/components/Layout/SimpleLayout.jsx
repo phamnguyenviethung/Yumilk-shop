a97326af07501373b8037e1e20e8606513bedae3
Viet Hung <viethung01234@gmail.com>
2024-06-13 09:35:22 +0700

    [UPDATE]: change logo

--- src/components/Layout/SimpleLayout.jsx
+++ src/components/Layout/SimpleLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { Center } from '@chakra-ui/react';
-
+import { Center, Image } from '@chakra-ui/react';
+import logo from '@/assets/logo.png';
 const SimpleLayout = () => {
   return (
     <>
-      <Center h='100px'>
-        <Center h='full'>Logo</Center>
+      <Center h='100px' mb={4}>
+        <Center h='full'>
+          <Image w='200px' src={logo} />
+        </Center>
       </Center>
       <Outlet />
     </>
