9523fb6a6ca1c27ff9ec7f64bfb6ba5a0a4c9449
kyanhngo <kyanh2501@gmail.com>
2024-06-16 02:00:54 +0700

    Edit Footer to make it look better

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Icon, Stack, Text, Link, VStack, HStack, Divider, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Icon, Stack, Text, Link, VStack, HStack, Divider, Grid, GridItem, Flex, Spacer, Image, Heading } from "@chakra-ui/react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import logo from '@/assets/logo.png';
 import { IoLocationOutline } from "react-icons/io5";
@@ -7,48 +7,67 @@ import { MdOutlineAlternateEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <Box>
-      <Divider my={'8'} />
-      <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} gap={'0'}>
-        <GridItem>
-          <Stack>
-            <Text fontWeight={'bold'}>Cửa hàng sữa cho mẹ bầu và em bé</Text>
-            <HStack>
-              <Icon as={IoLocationOutline} />
-              <Text fontWeight={'bold'}>Địa chỉ:</Text>
-              <Text>Thành phố Hồ Chí Minh</Text>
-            </HStack>
-            <HStack>
-              <Icon as={MdLocalPhone} />
-              <Text fontWeight={'bold'}>Liên hệ:</Text>
-              <Text>0123456789</Text>
-            </HStack>
-            <HStack>
-              <Icon as={MdOutlineAlternateEmail} />
-              <Text fontWeight={'bold'}>Email:</Text>
-              <Text>yumilk@gmail.com</Text>
-            </HStack>
-          </Stack>
-        </GridItem>
-        <GridItem>
-          <Stack>
-            <Text fontWeight={'bold'}>Về chúng tôi</Text>
-            <Link>Trang chủ</Link>
-            <Link>Giới thiệu</Link>
-            <Link>Liên hệ</Link>
-            <Link>Dịch vụ</Link>
-          </Stack>
-        </GridItem>
-        <GridItem>
-          <Stack>
-            <Text fontWeight={'bold'}>Hỗ trợ khách hàng</Text>
-            <Link>Tra cứu hoá đơn</Link>
-            <Link>Hình thức thanh toán</Link>
-          </Stack>
-        </GridItem>
-      </Grid>
-      <Divider my={'8'} />
-      <Text>© 2024 Yumilk  | All rights reserved</Text>
+    <Box as="footer"
+      borderTop="1px solid"
+      borderColor="gray.300"
+      py="2.5rem"
+      fontSize="0.875rem">
+      <Box maxW="64rem"
+        marginX="auto"
+        pb="2rem"
+        mb="1.5rem"
+        px={10}
+        borderBottom="1px solid"
+        borderColor="gray.300">
+        <Flex flexWrap={'wrap'} alignItems={'start'} justifyContent={'space-between'}>
+          <Box w={{ base: '100%', sm: '50%', md: 'max-content' }} mb={{ base: '1.5rem', lg: '0' }}>
+            <Image src={logo} boxSize='100px'></Image>
+            <Stack>
+              <Text>"Cửa hàng sữa cho mẹ bầu và em bé"</Text>
+              <HStack>
+                <Icon as={IoLocationOutline} />
+                <Text fontWeight={'bold'}>Địa chỉ:</Text>
+                <Text>Thành phố Hồ Chí Minh</Text>
+              </HStack>
+              <HStack>
+                <Icon as={MdLocalPhone} />
+                <Text fontWeight={'bold'}>Liên hệ:</Text>
+                <Text>0123456789</Text>
+              </HStack>
+              <HStack>
+                <Icon as={MdOutlineAlternateEmail} />
+                <Text fontWeight={'bold'}>Email:</Text>
+                <Text>yumilk@gmail.com</Text>
+              </HStack>
+            </Stack>
+          </Box>
+          <Box w={{ base: '100%', sm: '50%', md: 'max-content' }} mb={{ base: '1.5rem', lg: '0' }}>
+            <Heading color="gray.700" mb="0.5rem" fontSize="0.875rem" fontWeight="600">
+              Về chúng tôi
+            </Heading>
+            <Stack>
+              <Link>Trang chủ</Link>
+              <Link>Giới thiệu</Link>
+              <Link>Liên hệ</Link>
+              <Link>Dịch vụ</Link>
+            </Stack>
+          </Box>
+          <Box w={{ base: '100%', sm: '50%', md: 'max-content' }} mb={{ base: '1.5rem', lg: '0' }}>
+            <Heading color="gray.700" mb="0.5rem" fontSize="0.875rem" fontWeight="600">
+              Hỗ trợ khách hàng
+            </Heading>
+            <Stack>
+              <Link>Tra cứu hoá đơn</Link>
+              <Link>Hình thức thanh toán</Link>
+            </Stack>
+          </Box>
+        </Flex>
+      </Box>
+      <Flex maxW="64rem" mx="auto" alignItems="center" px={10}>
+        <Text color="gray.600" fontSize="0.875rem" pl="0.5rem">
+          &copy; 2024 Yumilk. All rights reserved.
+        </Text>
+      </Flex>
     </Box>
   );
 }
