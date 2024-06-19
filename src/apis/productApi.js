877f963a3aa125a88502d7440ff3bb064c970231
kyanhngo <kyanh2501@gmail.com>
2024-06-19 22:24:08 +0700

    Add TabInfo components

--- src/apis/productApi.js
+++ src/apis/productApi.js
@@ -13,7 +13,41 @@ export const productApi = api.injectEndpoints({
       transformResponse: res => res.data,
       providesTags: ['Product'],
     }),
+    getDescriptionInfoById: build.query({
+      query: id => ({
+        url: `/products/${id}`,
+      }),
+      transformResponse: res => res.data,
+      providesTags: ['Product'],
+    }),
+    getAttributeValueById: build.query({
+      query: id => ({
+        url: `/products/${id}/attributes/values`,
+      }),
+      transformResponse: res => res.data,
+      providesTags: ['Product'],
+    }),
+    getBrandInfoById: build.query({
+      query: id => ({
+        url: `/products/brands/${id}`,
+      }),
+      transformResponse: res => res.data,
+      providesTags: ['Product'],
+    }),
+    getFeedbackByProductId: build.query({
+      query: productId => ({
+        url: `/products/${productId}/reviews`,
+      }),
+      transformResponse: res => res.data,
+      providesTags: ['Product'],
+    }),
   }),
 });
 
-export const { useGetSellingProductQuery } = productApi;
+export const {
+  useGetSellingProductQuery,
+  useGetDescriptionInfoByIdQuery,
+  useGetAttributeValueByIdQuery,
+  useGetBrandInfoByIdQuery,
+  useGetFeedbackByProductIdQuery,
+} = productApi;
