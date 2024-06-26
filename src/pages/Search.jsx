16f7936bd9ef9570db2fe6c114a0e1e12d30bab2
Viet Hung <viethung01234@gmail.com>
2024-06-26 20:08:28 +0700

    [ADD]: search by keyword

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,20 @@
 import GridProductList from '@/features/Product/ProductList/GridProductList';
-import { Container } from '@chakra-ui/react';
+import { Box, Container, Text } from '@chakra-ui/react';
+import { useSearchParams } from 'react-router-dom';
 
 const Search = () => {
+  const [searchPararm] = useSearchParams();
   return (
     <Container maxW='container.xl'>
+      <Box textAlign='center' w='full' mb={4} fontSize='1.2rem'>
+        <Text>
+          Kết quả tìm kiếm <b>{searchPararm.get('keyword') || ''}</b>
+        </Text>
+      </Box>
       <GridProductList
         params={{
           pageSize: 30,
+          searchTerm: searchPararm.get('keyword') || '',
         }}
       />
     </Container>
