cad0acdfe58585e202ef046491ecd79d519468de
Viet Hung <viethung01234@gmail.com>
2024-06-27 10:11:43 +0700

    [ADD]: category and price filter

---
+++ src/features/Search/Filter/CategoryFilter.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { useGetAllCategoryQuery } from '@/apis/categoryApi';
+import { Checkbox, SimpleGrid } from '@chakra-ui/react';
+import { useState } from 'react';
+const CategoryFilter = ({ setFilterQuery, selectedData }) => {
+  const { data, isLoading } = useGetAllCategoryQuery();
+  const [categories, setCategories] = useState(selectedData);
+
+  if (isLoading) return <p>loading</p>;
+
+  const handleCheck = (categoryID, isChecked) => {
+    if (isChecked) {
+      setCategories(prev => {
+        prev.push(categoryID);
+        return prev;
+      });
+    } else {
+      setCategories(prev => prev.filter(id => id !== categoryID));
+    }
+
+    setFilterQuery(prev => {
+      return {
+        ...prev,
+        categoryIds: categories,
+      };
+    });
+  };
+
+  return (
+    <SimpleGrid columns={2} w='full' spacing={4}>
+      {data.items.map(category => {
+        return (
+          <Checkbox
+            isChecked={selectedData.includes(category.id)}
+            onChange={e => handleCheck(category.id, e.target.checked)}
+            key={category.id}
+            colorScheme='pink'
+          >
+            {category.name}
+          </Checkbox>
+        );
+      })}
+    </SimpleGrid>
+  );
+};
+
+export default CategoryFilter;
--- src/features/Search/Filter/index.jsx
+++ src/features/Search/Filter/index.jsx
@@ -2,6 +2,8 @@
 import {
   Box,
   Button,
+  Heading,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -10,20 +12,34 @@ import {
   ModalHeader,
   ModalOverlay,
   VStack,
+  filter,
   useDisclosure,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import BrandFilter from './BrandFilter';
+import FilterIcon from '@/assets/Icon/filter';
+import CategoryFilter from './CategoryFilter';
+import PriceFilter from './PriceFilter';
 
 const Filter = ({ setQueryStr }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [filterQuery, setFilterQuery] = useState({
     brandIds: [],
+    categoryIds: [],
+    minPrice: 0,
+    maxPrice: 1000000,
   });
 
   return (
     <>
-      <Button onClick={onOpen}>Bộ lọc</Button>
+      <Button
+        onClick={onOpen}
+        colorScheme='pink'
+        variant='outline'
+        leftIcon={<Icon as={FilterIcon} />}
+      >
+        Bộ lọc
+      </Button>
 
       <Modal
         onClose={onClose}
@@ -33,32 +49,54 @@ const Filter = ({ setQueryStr }) => {
           base: 'md',
           lg: 'lg',
         }}
+        scrollBehavior='inside'
       >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Bộ lọc</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <VStack w='full'>
+            <VStack w='full' spacing={2}>
               <Box w='full'>
+                <Heading as='h4' fontSize='0.9rem' my={4} fontWeight={600}>
+                  Nhãn hàng
+                </Heading>
                 <BrandFilter
                   selectedData={filterQuery.brandIds}
                   setFilterQuery={setFilterQuery}
                 />
               </Box>
-              <Box>category</Box>
-              <Box>price</Box>
+              <Box w='full'>
+                <Heading as='h4' fontSize='0.9rem' my={4} fontWeight={600}>
+                  Danh mục
+                </Heading>
+                <CategoryFilter
+                  selectedData={filterQuery.categoryIds}
+                  setFilterQuery={setFilterQuery}
+                />
+              </Box>
+              <Box w='full'>
+                <Heading as='h4' fontSize='0.9rem' my={4} fontWeight={600}>
+                  Giá tiền
+                </Heading>
+                <PriceFilter
+                  min={filterQuery.minPrice}
+                  max={filterQuery.maxPrice}
+                  setFilterQuery={setFilterQuery}
+                />
+              </Box>
               <Box>rate</Box>
             </VStack>
           </ModalBody>
           <ModalFooter>
             <Button
+              colorScheme='pink'
               onClick={() => {
                 setQueryStr(filterQuery);
                 onClose();
               }}
             >
-              Close
+              Xác nhận
             </Button>
           </ModalFooter>
         </ModalContent>
