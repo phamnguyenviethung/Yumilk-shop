ac942f1f61d92696a0d2e999f995d0394601ee52
kyanhngo <kyanh2501@gmail.com>
2024-07-12 17:14:49 +0700

    Add post component to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from '@/features/Banner';
 import BrandBanner from '@/features/Banner/BrandBanner';
+import SliderPostList from '@/features/Post/PostList/SliderPostList/SliderPostList';
 import GridProductList from '@/features/Product/ProductList/GridProductList';
 import SliderProductList from '@/features/Product/ProductList/SliderProductList';
 import { Box, Center, Container } from '@chakra-ui/react';
@@ -12,6 +13,14 @@ const Home = () => {
       <Center mb={4} pb={8}>
         <BrandBanner />
       </Center>
+      <Box>
+        <SliderPostList
+          heading='Tin tức mới nhất'
+          params={{
+            pageSize: 10,
+          }}
+        />
+      </Box>
       <Box>
         <SliderProductList
           heading='Đang giảm giá'
