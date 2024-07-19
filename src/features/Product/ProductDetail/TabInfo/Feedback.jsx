d03b5a7a9d60a5a615db2ec73cc30aa766e7b889
Viet Hung <viethung01234@gmail.com>
2024-07-20 06:55:25 +0700

    [UPDATE]: style for feedback box

--- src/features/Product/ProductDetail/TabInfo/Feedback.jsx
+++ src/features/Product/ProductDetail/TabInfo/Feedback.jsx
@@ -1,42 +1,45 @@
 /* eslint-disable react/prop-types */
-import {
-  Avatar,
-  Box,
-  Card,
-  CardBody,
-  Flex,
-  Stack,
-  Text,
-} from '@chakra-ui/react';
+import { Avatar, Box, HStack, Icon, Text, VStack } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import { PiStarFill } from 'react-icons/pi';
+
+const StarList = ({ rate }) => {
+  return (
+    <>
+      {[1, 2, 3, 4, 5].map(n => {
+        if (n > rate) return <></>;
+        return (
+          <Icon key={n} as={PiStarFill} color='yellow.500' fontSize='1.1rem' />
+        );
+      })}
+    </>
+  );
+};
 
 function Feedback({ feedback }) {
-  // Convert the date string to a Date object
-  const date = new Date(feedback?.createdAt);
-
-  // Format the date
-  const formattedDate = `${
-    date.getMonth() + 1
-  }/${date.getDate()}/${date.getFullYear()}`;
-  console.log(feedback?.createdAt);
-
+  console.log(feedback);
   return (
-    <Card>
-      <CardBody>
-        <Flex>
-          <Avatar />
-          <Box marginLeft={'13px'}>
-            <Stack direction={'column'}>
-              <h3>{feedback?.id}</h3>
-              <Stack direction={'row'}>
-                <p>{feedback?.rating} stars</p>
-                <p>{formattedDate}</p>
-              </Stack>
-              <Text>{feedback?.review}</Text>
-            </Stack>
-          </Box>
-        </Flex>
-      </CardBody>
-    </Card>
+    <HStack gap='2'>
+      <Avatar />
+      <Box>
+        <VStack w='full' alignItems='flex-start'>
+          <Text fontSize='1.1rem' fontWeight={600}>
+            {feedback?.firstName} {feedback?.lastName}
+          </Text>
+          <HStack>
+            <HStack gap='1'>
+              <StarList rate={feedback?.rating} />{' '}
+            </HStack>
+            <Text color='gray.600' fontWeight={500} fontSize='0.85rem'>
+              {dayjs(feedback?.createdAt).format('HH:mm  DD/MM/YYYY')}
+            </Text>
+          </HStack>
+          <Text fontSize='1.2rem' fontWeight={500} fontStyle='italic'>
+            {feedback?.review}
+          </Text>
+        </VStack>
+      </Box>
+    </HStack>
   );
 }
 
--- src/features/Product/ProductDetail/TabInfo/ReviewTab.jsx
+++ src/features/Product/ProductDetail/TabInfo/ReviewTab.jsx
@@ -1,10 +1,19 @@
 import { useGetFeedbackByProductIdQuery } from '@/apis/productApi';
 import { useParams } from 'react-router-dom';
 import Feedback from './Feedback';
+import { Center, Text } from '@chakra-ui/react';
 
 const ReviewTab = () => {
   const { id } = useParams();
   const { data: feedback } = useGetFeedbackByProductIdQuery(id);
+
+  if (feedback?.items?.length === 0) {
+    return (
+      <Center boxSize='full'>
+        <Text>Không có đánh giá nào</Text>
+      </Center>
+    );
+  }
   return feedback?.items.map(item => (
     <Feedback key={item.id} feedback={item} />
   ));
