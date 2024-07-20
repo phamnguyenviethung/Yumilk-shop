b32970578cce786379a4ea385073b98c088dcf71
Viet Hung <viethung01234@gmail.com>
2024-07-20 09:24:26 +0700

    [ADD]: product widget  in post detail page

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,5 +1,6 @@
 import { useGetPostDetailQuery } from '@/apis/postApi';
 import PostContent from '@/features/Post/PostDetail/PostContent';
+import SliderProductList from '@/features/Product/ProductList/SliderProductList';
 import { Box, Container } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 
@@ -14,6 +15,16 @@ const PostDetail = () => {
     <Container maxW='container.xl' py={8}>
       <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p={6}>
         {postData && <PostContent postData={postData} />}
+      </Box>
+      <Box>
+        <SliderProductList
+          heading='Đang giảm giá'
+          params={{
+            onSale: true,
+            pageSize: 10,
+            statusIds: [1],
+          }}
+        />
       </Box>
     </Container>
   );
