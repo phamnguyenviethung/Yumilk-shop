b2265c8fc80a285eff202c5136f49f81d4462ba7
Viet Hung <viethung01234@gmail.com>
2024-07-20 11:19:38 +0700

    [UPDATE]: brand info ui style

--- src/features/Product/ProductDetail/TabInfo/BrandTab.jsx
+++ src/features/Product/ProductDetail/TabInfo/BrandTab.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable react/prop-types */
 import { useGetBrandInfoByIdQuery } from '@/apis/productApi';
+import { Box, HStack, Image, Text } from '@chakra-ui/react';
 
 const BrandTab = ({ brandId }) => {
-  const brandInfo = useGetBrandInfoByIdQuery(brandId);
+  const { data } = useGetBrandInfoByIdQuery(brandId);
   return (
-    <div>
-      <h1>{brandInfo.data?.name}</h1>
-      <p>{brandInfo.data?.description}</p>
-    </div>
+    <Box w='full'>
+      <HStack w='full' gap='2'>
+        <Image src={data.logo} boxSize={[100, 120]} />
+        <Text fontWeight={700} color='pink.400' fontSize='1.5rem'>
+          {data.name}
+        </Text>
+      </HStack>
+      <Text fontWeight={700} color='pink.400' fontSize='1.5rem'>
+        {data.description}
+      </Text>
+    </Box>
   );
 };
 
