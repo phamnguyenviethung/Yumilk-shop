2b9b23630c372b771b567e9a70ad6b839c956955
Viet Hung <viethung01234@gmail.com>
2024-07-21 16:35:54 +0700

    [UPDATE]: brand text ui

--- src/features/Product/ProductDetail/TabInfo/BrandTab.jsx
+++ src/features/Product/ProductDetail/TabInfo/BrandTab.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
 import { useGetBrandInfoByIdQuery } from '@/apis/productApi';
-import { Box, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, Divider, HStack, Image, Text, VStack } from '@chakra-ui/react';
 
 const BrandTab = ({ brandId }) => {
   const { data } = useGetBrandInfoByIdQuery(brandId);
   return (
     <Box w='full'>
-      <HStack w='full' gap='2'>
+      <VStack gap='4' mb={8}>
         <Image src={data.logo} boxSize={[100, 120]} />
-        <Text fontWeight={700} color='pink.400' fontSize='1.5rem'>
+
+        <Text fontWeight={700} color='pink.400' fontSize='2rem'>
           {data.name}
         </Text>
-      </HStack>
-      <Text fontWeight={700} color='pink.400' fontSize='1.5rem'>
+      </VStack>
+      <Divider />
+      <Text fontWeight={500} fontStyle='italic' fontSize='1rem'>
         {data.description}
       </Text>
     </Box>
